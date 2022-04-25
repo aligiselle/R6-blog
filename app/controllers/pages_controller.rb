08cff0ab2695ff88faaf25d530084cb37d237cc6
aligiselle <alishagiselle@gmail.com>
2022-04-25 11:52:43 -0400

    Lesson 3.1 Rails Basic Complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,60 @@
 class PagesController < ApplicationController
+    #before_action :set_page, except: [:index, :new, :create]
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+
     def index
         @pages = Page.all
     end
+
+    def show
+        set_page
+        #@page = Page.find(params[:id])
+        #render plain: @page.title
+        #render plain: params[:id]
+    end
+
+    def new
+        @page = Page.new
+    end
+
+    def create
+        #page_params = params.require(:page).permit(:title, :body, :slug)
+        @page = Page.new(page_params)
+        @page.save
+        redirect_to @page
+        #@page = Page.new(params)
+        #render plain: params.hash
+        #render plain: params.to_json
+    end
+
+    def edit
+        set_page
+        #@page = Page.find(params[:id])
+    end
+
+    def update
+        set_page
+        #@page = Page.find(params[:id])
+        #page_params = params.require(:page).permit(:title, :body, :slug)
+        @page.update(page_params)
+        redirect_to @page
+    end
+    
+    def destroy
+        set_page
+        #@page = Page.find(params[:id])
+        @page.destroy
+        redirect_to pages_path
+    end
+
+
+    private
+        def page_params
+            params.require(:page).permit(:title, :body, :slug)
+        end
+
+        def set_page
+            @page = Page.find(params[:id])
+        end
+
 end
